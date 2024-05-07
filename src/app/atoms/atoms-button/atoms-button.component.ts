62c6a437a981b680593ddb00e585ebcd74f0cf87
Parvin-Gasimli <parvinzga@code.edu.az>
2024-05-07 17:15:58 +0400

    add angluar atoms buttons

--- src/app/atoms/atoms-button/atoms-button.component.ts
+++ src/app/atoms/atoms-button/atoms-button.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-atoms-button',
   standalone: true,
   imports: [],
   templateUrl: './atoms-button.component.html',
-  styleUrl: './atoms-button.component.scss'
+  styleUrl: './atoms-button.component.scss',
 })
 export class AtomsButtonComponent {
-
+  @Input() label: string = 'Button';
+  @Input() Isicon?: boolean;
+  @Input() disabled: boolean = false;
+  @Input() icon?: string = '';
 }
